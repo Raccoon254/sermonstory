fef42999544a371cb974f310f9393bbe854c05bf
Steve Tom <tomsteve187@gmail.com>
2023-10-13 21:42:38 +0300

    Fixed the prompts migration

--- database/migrations/2023_10_13_184127_create_prompts_table.php
+++ database/migrations/2023_10_13_184127_create_prompts_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('prompts', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->text('content');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
