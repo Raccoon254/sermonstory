b241851955bd5c32b5c0d6399dbe8eb90a660075
Steve Tom <tomsteve187@gmail.com>
2023-11-05 22:27:55 +0300

    Uncommented comments on DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,13 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-
         //call categories seeder
         $this->call(CategoryTagSeeder::class);
         //call stories seeder
