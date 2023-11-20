65d1d0bc36ada1d245c356d79b93c045c8db0579
Raccoon254 <tomsteve187@gmail.com>
2023-11-20 16:23:57 +0300

    Btn cleanup

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,15 +1,15 @@
-<div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+<div class="flex gap-2 flex-col sm:flex-row fixed top-0 right-0 p-4 text-right z-10">
     @auth
         <a data-tip="Dashboard" href="{{ url('/dashboard') }}" class="btn text-gray-50 rounded btn-outline ring ring-inset font-semibold focus:outline-none focus:ring-2 focus:ring-red-500" wire:navigate>
             <i class="fas fa-tachometer-alt mr-1"></i> Dashboard
         </a>
     @else
-        <a href="{{ route('login') }}" class="btn text-gray-50 rounded btn-outline ring ring-inset font-semibold focus:outline-none focus:ring-2 focus:ring-red-500" wire:navigate>
+        <a href="{{ route('login') }}" class="btn w-full sm:w-auto text-gray-50 rounded btn-outline ring ring-inset font-semibold focus:outline-none focus:ring-2 focus:ring-red-500" wire:navigate>
             <i class="fas fa-sign-in-alt mr-1"></i> Log in
         </a>
 
         @if (Route::has('register'))
-            <a href="{{ route('register') }}" class="ml-4 btn text-gray-50 rounded btn-outline ring ring-inset font-semibold focus:outline-none focus:ring-2 focus:ring-red-500" wire:navigate>
+            <a href="{{ route('register') }}" class="w-full sm:w-auto btn text-gray-50 rounded btn-outline ring ring-inset font-semibold focus:outline-none focus:ring-2 focus:ring-red-500" wire:navigate>
                 <i class="fas fa-user-plus mr-1"></i> Register
             </a>
         @endif
